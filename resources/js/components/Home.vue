f4ec4bc2fedb33c8f12f27bbb7086703e517e19a
frittesauce <9026879@student.zadkine.nl>
2024-12-17 09:25:56 +0100

    sasdada

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -1,6 +1,35 @@
-<script>
+<script setup>
+import { onMounted } from 'vue';
+
+
+async function takePicture(event) {
+
+  let mediaDevices = navigator.mediaDevices;
+  let video = document.getElementById("vid");
+  vid.muted = true;
+
+  mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: true,
+    })
+    .then((stream) => {
+      // Changing the source of video to current stream.
+      video.srcObject = stream;
+      video.addEventListener("loadedmetadata", () => {
+        video.play();
+      });
+    })
+    .catch(alert);
+
+}
 </script>
 
 <template>
   <p>test</p>
+  <input type="file" accept="image/*" capture="camera">
+
+  <div>
+    <video id="vid"></video>
+  </div>
 </template>
