44728b1504afaded5ae3405d0269743f1408f42c
frittesauce <9026879@student.zadkine.nl>
2024-12-17 08:53:19 +0100

    commmit

--- resources/js/components/AuraMap.vue
+++ resources/js/components/AuraMap.vue
@@ -10,8 +10,12 @@ import L from 'leaflet';
 export default {
   name: 'AuraMap',
   mounted() {
+
+    let cords = ["51.93", "4.48"]
+
+
     // Initialize the map when the component is mounted
-    const map = L.map('map').setView(cords, 20); // Initial coordinates [latitude, longitude], zoom level
+    const map = L.map('map').setView(cords, 12); // Initial coordinates [latitude, longitude], zoom level
 
     // Add OpenStreetMap tile layer
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
