eece9d48f9a3ecda77ad232d9b2b21b3951ce977
frittesauce <9026879@student.zadkine.nl>
2024-12-18 12:52:55 +0100

    blah

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -2,18 +2,55 @@
 </script>
 
 <template>
-  <section id="home">
+  <div class="homepageBox w-dvw h-[80vh]">
+    <section id="home">
 
-    <div class="w-dvw h-[70vh]">
-      <div class=" gap-8 flex justify-center sm:flex-row flex-col items-center w-full h-full">
-        <img
-          src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.paradefloatstuff.com%2Fwp-content%2Fuploads%2F2019%2F03%2Flet12g_sigma.jpg&f=1&nofb=1&ipt=27f1b5fa9b4ea67ff4999d84f576482e0832932dfc90143977c30d8e86efcddb&ipo=images"
-          alt="plaatje van een sigma" width="320" height="320" class=" rounded-xl" />
-        <div class="gap-y-4 flex flex-col w-96 h-96 justify-center">
-          <h1 class="text-4xl font-semibold">Welcome tot Aura-Fixer</h1>
-          <p class="max-w-96 text-base">Geef Sigmavile de winterarc die we nodig hebben!</p>
+      <div class="w-dvw h-[70vh]">
+        <div class=" gap-8 flex justify-center sm:flex-row flex-col items-center w-full h-full">
+          <img
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.paradefloatstuff.com%2Fwp-content%2Fuploads%2F2019%2F03%2Flet12g_sigma.jpg&f=1&nofb=1&ipt=27f1b5fa9b4ea67ff4999d84f576482e0832932dfc90143977c30d8e86efcddb&ipo=images"
+            alt="plaatje van een sigma" width="320" height="320" class=" rounded-xl" />
+          <div class="gap-y-4 flex flex-col w-96 h-96 justify-center">
+            <h1 class="text-4xl font-semibold">Welcome tot Aura-Fixer</h1>
+            <p class="max-w-96 text-base">Geef Sigmavile de winterarc die we nodig hebben!</p>
+          </div>
         </div>
       </div>
-    </div>
-  </section>
+    </section>
+    <section id="2">
+
+      <div class="w-dvw h-[70vh]">
+        <div class=" gap-8 flex justify-center sm:flex-row flex-col items-center w-full h-full">
+          <img
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.paradefloatstuff.com%2Fwp-content%2Fuploads%2F2019%2F03%2Flet12g_sigma.jpg&f=1&nofb=1&ipt=27f1b5fa9b4ea67ff4999d84f576482e0832932dfc90143977c30d8e86efcddb&ipo=images"
+            alt="plaatje van een sigma" width="320" height="320" class=" rounded-xl" />
+          <div class="gap-y-4 flex flex-col w-96 h-96 justify-center">
+            <h1 class="text-4xl font-semibold">Welcome tot 2</h1>
+            <p class="max-w-96 text-base">Geef Sigmavile de winterarc die we nodig hebben!</p>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section id="3">
+
+      <div class="w-dvw h-[70vh]">
+        <div class=" gap-8 flex justify-center sm:flex-row flex-col items-center w-full h-full">
+          <img
+            src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.paradefloatstuff.com%2Fwp-content%2Fuploads%2F2019%2F03%2Flet12g_sigma.jpg&f=1&nofb=1&ipt=27f1b5fa9b4ea67ff4999d84f576482e0832932dfc90143977c30d8e86efcddb&ipo=images"
+            alt="plaatje van een sigma" width="320" height="320" class=" rounded-xl" />
+          <div class="gap-y-4 flex flex-col w-96 h-96 justify-center">
+            <h1 class="text-4xl font-semibold">Welcome tot 3</h1>
+            <p class="max-w-96 text-base">Geef Sigmavile de winterarc die we nodig hebben!</p>
+          </div>
+        </div>
+      </div>
+    </section>
+
+  </div>
 </template>
+
+<style lang="css">
+.homepageBox {
+  overflow-y: hidden;
+}
+</style>
