e8f5a7285f9eead4fb51225e006bfc2dd72b6b5f
frittesauce <9026879@student.zadkine.nl>
2024-12-18 11:39:46 +0100

    updates home page

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -2,5 +2,15 @@
 </script>
 
 <template>
-  <p>test</p>
+  <section id="home">
+    <div class=" flex w-full p-32 justify-center items-center">
+      <img
+        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fstatic.wikia.nocookie.net%2Fskibidi-toilet-official%2Fimages%2F9%2F99%2FNormalToiletImage.png%2Frevision%2Flatest%3Fcb%3D20230921103252&f=1&nofb=1&ipt=c9fb45bff53e27ec4cc0aa25d2898e27dce5433251e1ff4b77ac593998f1d974&ipo=images"
+        width="320" height="320">
+      <div>
+        <h1>gemeente rizz!</h1>
+        <p>de meest sigma stad</p>
+      </div>
+    </div>
+  </section>
 </template>
