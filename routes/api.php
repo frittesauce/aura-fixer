072836f7acb88ee67f544e825aebe95269941471
frittesauce <9026879@student.zadkine.nl>
2024-12-19 12:16:51 +0100

    added the api thingy

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/post', [\App\Http\Controllers\DBController::class, 'write'])
     ->middleware(\App\Http\Middleware\CheckData::class);
+
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::post('/post', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
